a73fa8db595ce02dda72e5493b190b08fad148a5
Butchy <52023302+pwnageByButchy@users.noreply.github.com>
2019-12-12 16:56:04 +1030

    Update UpdateWithPS.ps1

--- UpdateScripts/UpdateWithPS.ps1
+++ UpdateScripts/UpdateWithPS.ps1
@@ -1,1 +1,17 @@
+    #Define update criteria.
+    $Criteria = "IsInstalled=0"
 
+    #Search for relevant updates.
+    $Searcher = New-Object -ComObject Microsoft.Update.Searcher
+    $SearchResult = $Searcher.Search($Criteria).Updates
+
+    #Download updates.
+    $Session = New-Object -ComObject Microsoft.Update.Session
+    $Downloader = $Session.CreateUpdateDownloader()
+    $Downloader.Updates = $SearchResult
+    $Downloader.Download()
+    $Installer = New-Object -ComObject Microsoft.Update.Installer
+    $Installer.Updates = $SearchResult
+    $Result = $Installer.Install()
+
+    If ($Result.rebootRequired) { shutdown.exe /t 0 /r }
