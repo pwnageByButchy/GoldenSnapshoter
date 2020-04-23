c762e93505ada7378d1eb0f17eb718c7bf74440b
pwnageByButchy <butch@loubartdesigns.com>
2020-04-23 14:56:40 +0930

    update script checked and working on Kali linux

--- UpdateScripts/UpdateWithAPT.py
+++ UpdateScripts/UpdateWithAPT.py
@@ -1,5 +1,5 @@
-
 import os
+import time
 import subprocess
 
 
@@ -10,33 +10,35 @@ import subprocess
 
 get_guest_os = 'cat /etc/*release | grep ^"NAME=" | cut -d "=" -f2 | tr \'"\' \' \'| cut -d " " -f2'
 result = subprocess.check_output(get_guest_os, shell=True)
-result = result.replace("b'", "")
-result = result.replace("\\n'", "")
+result = result.decode("utf-8")
+result = result.strip()
 
 
 def switch(value):
-    return {
-        "Kali": 1,
-        "Ubuntu": 1,
-        "Parrot": 1,
-        "RHEL": 2,
-        "CentOS": 2,
-        "Fedora": 2,
-        "Arch": 3,
-        "blackarch": 3,
-    }.get(value, 42)
+    if value in apt:
+        apt_update()
+    elif value in yum:
+        yum_update()
+    elif value in pacman:
+        pacman_update()
+    else:
+        print("This value is not accounted for " + value)
+        print("Unable to update system")
+ 
 
 
 def apt_update():
     os.system('apt-get update -y && apt-get upgrade -y')
     os.system('apt-get autoclean -y && apt-get clean -y')
     os.system('apt-get autoremove -y')
+    time.sleep(10)
     os.system('poweroff')
 
 
 def yum_update():
     os.system('yum check-update && yum -y update')
     os.system('yum -y autoremove')
+    time.sleep(10)
     os.system('poweroff')
 
 
@@ -44,14 +46,13 @@ def pacman_update():
     # Make sure the network interface is up
     os.system('systemctl restart NetworkManager')
     os.system('pacman -Syu --noconfirm')
+    time.sleep(10)
     os.system('poweroff')
 
+apt = ["Kali", "Ubuntu", "Parrot", "Debian"]
+yum = ["RHEL", "CentOS", "Fedora"]
+pacman = ["blackarch", "arch"]
 
-if switch(result) == 1:
-    apt_update()
-elif switch(result) == 2:
-    yum_update()
-elif switch(result) == 3:
-    pacman_update()
-else:
-    print("Unable to update system")
+
+switch(result)
+
