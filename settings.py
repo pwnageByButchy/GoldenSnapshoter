7362447ba1c319dd48ddf0ed3232b7f6be3332d5
pwnageByButchy <butch@loubartdesigns.com>
2020-06-02 13:54:06 +0930

    adding settings

---
+++ settings.py
@@ -0,0 +1,53 @@
+class Settings:
+    def __init__(self):
+        # are you using vmware or virtualbox?
+        self.virtualisation_platform = r'vmware'
+
+        # What VMWare Product are you using ws = Workstation, fusion = VMWare Fusion, esx = ESX/ESXi hosts
+        self.vmware_product = r'ws'
+
+        # Directory of the VMs you wish to snapshot
+        self.vm_directory = r'I:\Virtual Machines\Clients'
+
+        # Extension VMware (.vmx) or VirtualBox (.vbox)
+        self.extension = r'.vmx'
+
+        # Name of your base image
+        self.base_image = r'baseImage'
+
+        # Linux GuestOS username
+        self.linux_guestuser = r'root'
+        # Linux GuestOS password
+        self.linux_guestpass = r'password'
+        # Linux path to where scripts go
+        self.linux_scripts_path_in_guest = r'/root'
+
+        # Windows GuestOS username
+        self.windows_guestuser = r'adminsitrator'
+        # Windows GuestOS password
+        self.windows_guestpass = r'password'
+        # Windows path to where scripts go
+        self.windows_scripts_path_in_guest = r'C:\Windows\temp'
+
+        # About Me and My Stuff
+        # I want to preserve my machine for forensic evidence as Reverting the VM will destroy any logs or files created
+        # Set to True if you want to presverse this in your snapshot...meaning it will be snapshotted before being reverted
+        # Default is False (NOT to preserve this data)
+        #
+        # This is handy for Forensic Investigators, Incident Management and Evidence requirements
+        self.preserve_for_forensic_evidence = False
+        # Clone VM Directory for when above question is True
+        self.preserve_forensic_clone_directory = r'I:\Virtual Machines\Clones'
+
+        # Your Git repo local folder location inside the guest
+        self.my_git_directory = r'/home/butchy/InternalGit'
+        # Third party Git Repo local folder location inside the guest
+        self.external_git_directory = r'/home/butchy/ExternalGit'
+        # List of My Git Repos to be cloned into my_git_directory
+        self.my_git_repos = {r'GoldenSnapshoter': r'https://github.com/pwnageByButchy/GoldenSnapshoter.git',
+                             r'UsefulShit': r'https://github.com/pwnageByButchy/UsefulShit.git'}
+        # List of Third Party Git Repos to be cloned into external_git_directory
+        self.external_git_repos = {r'USB-Rubber-Ducky': r'https://github.com/hak5darren/USB-Rubber-Ducky.git',
+                                   r'Bash-Bunny-Payloads': r'https://github.com/hak5darren/bashbunny-payloads.git',
+                                   r'Packet-Squirrel-Payloads': r'https://github.com/hak5darren/packetsquirrel-payloads.git',
+                                   r'StegCracker': r'https://github.com/Paradoxis/StegCracker.git'}
