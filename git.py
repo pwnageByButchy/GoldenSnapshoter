8bfd425dff8c2fe448ca28a47225a47602634454
pwnageByButchy <butch@loubartdesigns.com>
2020-06-10 16:52:52 +0930

    Git generation script created and tested

--- git.py
+++ git.py
@@ -11,32 +11,39 @@ class Git:
         app_path = os.path.abspath(os.path.dirname(sys.argv[0]))
         git_file = app_path + '\\UpdateScripts\\git_script.txt'
         file = open(git_file, "w")
+        file.write("import shutil\n\n")
         file.write("#### Copy from below here and paste it into whichever Update Script you want to add it too ####\n")
         # Create MyGit Repo folder
-        file.write("if not os.path.exists({0}):\n".format(self.my_settings.my_git_directory))
-        file.write("    os.makedirs({0})\n".format(self.my_settings.my_git_directory))
-        file.write("os.chdir({0})\n\n".format(self.my_settings.my_git_directory))
+        file.write("if not os.path.exists('{0}'):\n".format(self.my_settings.my_git_directory))
+        file.write("    os.makedirs('{0}')\n".format(self.my_settings.my_git_directory))
         # for loop looping through My Git Repos dictionary
         for repo_name in self.my_settings.my_git_repos:
-            file.write("if not os.path.exists({0}):\n".format(repo_name))
-            file.write("    git clone {0} {1})\n".format(self.my_settings.my_git_repos[repo_name], repo_name))
+            file.write("if not os.path.exists('{0}/{1}'):\n".format(self.my_settings.my_git_directory, repo_name))
+            file.write("    os.system('git clone {0} {1}/{2}')\n".format(
+                self.my_settings.my_git_repos[repo_name], self.my_settings.my_git_directory, repo_name))
         # if exists delete repo then git cloned
             file.write("else:\n")
-            file.write("    os.rmdir({0})\n".format(repo_name))
-            file.write("    git clone {0} {1})\n\n".format(self.my_settings.my_git_repos[repo_name], repo_name))
+            file.write("    shutil.rmtree('{0}/{1}')\n".format(self.my_settings.my_git_directory, repo_name))
+            file.write("    os.system('git clone {0} {1}/{2}')\n\n".format(
+                self.my_settings.my_git_repos[repo_name], self.my_settings.my_git_directory, repo_name))
 
         # Create 3rd Party Repo Directory
-        file.write("if not os.path.exists({0}):\n".format(self.my_settings.external_git_directory))
-        file.write("    os.makedirs({0})\n".format(self.my_settings.external_git_directory))
-        file.write("os.chdir({0})\n\n".format(self.my_settings.external_git_directory))
+        file.write("if not os.path.exists('{0}'):\n".format(self.my_settings.external_git_directory))
+        file.write("    os.makedirs('{0}')\n".format(self.my_settings.external_git_directory))
+
         # for loop looping through External Git Repos dictionary
         for repo_name in self.my_settings.external_git_repos:
-            file.write("if not os.path.exists({0}):\n".format(repo_name))
-            file.write("    git clone {0} {1})\n".format(self.my_settings.external_git_repos[repo_name], repo_name))
+            file.write("if not os.path.exists('{0}/{1}'):\n".format(self.my_settings.external_git_directory, repo_name))
+            file.write("    os.system('git clone {0} {1}/{2}')\n".format(
+                self.my_settings.external_git_repos[repo_name], self.my_settings.external_git_directory, repo_name))
         # if exists delete repo then git cloned
             file.write("else:\n")
-            file.write("    os.rmdir({0})\n".format(repo_name))
-            file.write("    git clone {0} {1})\n\n".format(self.my_settings.external_git_repos[repo_name], repo_name))
-
-        file.write("#### End Copying here ####\n")
+            file.write("    shutil.rmtree('{0}/{1}')\n".format(self.my_settings.external_git_directory, repo_name))
+            file.write("    os.system('git clone {0} {1}/{2}')\n\n".format(
+                self.my_settings.external_git_repos[repo_name], self.my_settings.external_git_directory, repo_name))
+        file.write("os.system('chown -R {0}:{0} {1}\n')".format(self.my_settings.linux_standard_user,
+                                                                self.my_settings.my_git_directory))
+        file.write("os.system('chown -R {0}:{0} {1}\n')".format(self.my_settings.linux_standard_user,
+                                                                self.my_settings.external_git_directory))
+        file.write("#### End Copying here ####\n\n")
         file.close()
--- settings.py
+++ settings.py
@@ -15,10 +15,12 @@ class Settings:
         # Name of your base image
         self.base_image = r'baseImage'
 
-        # Linux GuestOS username
+        # Linux GuestOS root username
         self.linux_guestuser = r'root'
-        # Linux GuestOS password
+        # Linux GuestOS root password
         self.linux_guestpass = r'password'
+        # standard user username
+        self.linux_standard_user = r'butchy'
         # Linux path to where scripts go
         self.linux_scripts_path_in_guest = r'/root'
 
@@ -44,8 +46,9 @@ class Settings:
         # Third party Git Repo local folder location inside the guest
         self.external_git_directory = r'/home/butchy/ExternalGit'
         # List of My Git Repos to be cloned into my_git_directory
-        self.my_git_repos = {r'GoldenSnapshoter': r'https://github.com/pwnageByButchy/GoldenSnapshoter.git',
-                             r'UsefulShit': r'https://github.com/pwnageByButchy/UsefulShit.git'}
+        # for private repos you will need to include a username and password in the URL
+        # example git clone https://username:password@github.com/username/repository.git
+        self.my_git_repos = {r'GoldenSnapshoter': r'https://github.com/pwnageByButchy/GoldenSnapshoter.git'}
         # List of Third Party Git Repos to be cloned into external_git_directory
         self.external_git_repos = {r'USB-Rubber-Ducky': r'https://github.com/hak5darren/USB-Rubber-Ducky.git',
                                    r'Bash-Bunny-Payloads': r'https://github.com/hak5darren/bashbunny-payloads.git',
