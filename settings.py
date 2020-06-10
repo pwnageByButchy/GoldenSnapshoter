b74472564d6402e9cdc0f31e6175afef54a72505
pwnageByButchy <butch@loubartdesigns.com>
2020-06-10 17:09:51 +0930

    updated instructions

--- settings.py
+++ settings.py
@@ -31,7 +31,6 @@ class Settings:
         # Windows path to where scripts go
         self.windows_scripts_path_in_guest = r'C:\Windows\temp'
 
-        # About Me and My Stuff
         # I want to preserve my machine for forensic evidence as Reverting the VM will destroy any logs or files created
         # Set to True if you want to presverse this in your snapshot...meaning it will be snapshotted before being reverted
         # Default is False (NOT to preserve this data)
@@ -41,14 +40,16 @@ class Settings:
         # Clone VM Directory for when above question is True
         self.preserve_forensic_clone_directory = r'I:\Virtual Machines\Clones'
 
+        # Your Stuff
         # Your Git repo local folder location inside the guest
         self.my_git_directory = r'/home/butchy/InternalGit'
+        # List of your Git Repos to be cloned into my_git_directory
+        # for private repos you will need to include a username and password in the URL
+        # example: https://username:password@github.com/username/repository.git
+        self.my_git_repos = {r'GoldenSnapshoter': r'https://github.com/pwnageByButchy/GoldenSnapshoter.git'}
+
         # Third party Git Repo local folder location inside the guest
         self.external_git_directory = r'/home/butchy/ExternalGit'
-        # List of My Git Repos to be cloned into my_git_directory
-        # for private repos you will need to include a username and password in the URL
-        # example git clone https://username:password@github.com/username/repository.git
-        self.my_git_repos = {r'GoldenSnapshoter': r'https://github.com/pwnageByButchy/GoldenSnapshoter.git'}
         # List of Third Party Git Repos to be cloned into external_git_directory
         self.external_git_repos = {r'USB-Rubber-Ducky': r'https://github.com/hak5darren/USB-Rubber-Ducky.git',
                                    r'Bash-Bunny-Payloads': r'https://github.com/hak5darren/bashbunny-payloads.git',
